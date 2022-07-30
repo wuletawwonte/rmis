a81250c8e407e397845d9d2cf001c9ba917d14d8
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-07-30 09:00:40 +0300

    Create new layout for login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "login"
+    else
+      "application"
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  layout "login", :only => [ :sign_in ]
+
   def index; end
 
 end
