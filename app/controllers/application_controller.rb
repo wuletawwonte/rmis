200fda3647aa29180158f26d16b097389ccd6357
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-07-31 03:37:49 +0300

    Add sign_out button in the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,11 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+
+  protected
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "users#index"
+
+  delete '/users/sign_out', to: 'users#sign_out'
 end
