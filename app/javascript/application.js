98754e3105d92789ff549495afa7029f69ca2724
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-14 17:01:49 +0300

    Add alpine and initAlpine to the project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import Alpine from "alpinejs"
+
+window.Alpine = Alpine 
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  window.Alpine.start(); 
+});
