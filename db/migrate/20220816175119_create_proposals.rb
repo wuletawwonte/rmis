5a27fb408c970dff57a0d60fc1c3f75290d8370c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-16 20:52:25 +0300

    Generate model with belongs to association

---
+++ db/migrate/20220816175119_create_proposals.rb
@@ -0,0 +1,13 @@
+class CreateProposals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :proposals do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.string :attachement
+      t.string :theme
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_210655) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_175119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "proposals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.string "attachement"
+    t.string "theme"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_proposals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_210655) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "proposals", "users"
 end
