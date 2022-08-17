4ddc53c74546e033316baa2378a750a6f288e5e5
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-17 19:52:38 +0300

    Generate Theme controller with index create new and destroy action

---
+++ app/controllers/theme_controller.rb
@@ -0,0 +1,13 @@
+class ThemeController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'theme/index'
+  get 'theme/create'
+  get 'theme/new'
+  get 'theme/destroy'
   resources :research_centers, only: %i[index show create new edit destroy]
   resources :proposals, only: %i[index show new create]
   devise_for :users
