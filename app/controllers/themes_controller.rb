631df661e4953e0c8e3775286d3897d0d54eeb1a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-17 20:57:24 +0300

    Imlement thematic area creation

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,6 @@
 class ThemesController < ApplicationController
   def index
-    @themes = Theme.all.includes(:research_centers)
+    @themes = Theme.all.includes(:research_center)
   end
 
   def new
@@ -10,6 +10,7 @@ class ThemesController < ApplicationController
 
   def create
     @theme = Theme.new(theme_params)
+    @theme.user_id = current_user.id
     
     if @theme.save
       redirect_to themes_path, notice: 'Successfully added.'
@@ -24,6 +25,6 @@ class ThemesController < ApplicationController
   private
 
   def theme_params
-    params.resource(:theme).permit(:title, :description)
+    params.require(:theme).permit(:title, :description, :research_center_id)
   end
 end
