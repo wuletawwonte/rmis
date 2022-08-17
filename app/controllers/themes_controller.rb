41199649afc880e7e3745004b87a90ae0ec9042d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-17 20:31:07 +0300

    Add menu for index and new actions of themes controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,13 +1,29 @@
 class ThemesController < ApplicationController
   def index
-  end
-
-  def create
+    @themes = Theme.all.includes(:research_centers)
   end
 
   def new
+    @theme = Theme.new
+    @research_centers = ResearchCenter.all
+  end
+
+  def create
+    @theme = Theme.new(theme_params)
+    
+    if @theme.save
+      redirect_to themes_path, notice: 'Successfully added.'
+    else
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def theme_params
+    params.resource(:theme).permit(:title, :description)
   end
 end
