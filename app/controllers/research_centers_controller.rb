6689a9423e71a6f6635cebeffa23f96a65050cac
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-17 14:38:07 +0300

    Add reserach center

--- app/controllers/research_centers_controller.rb
+++ app/controllers/research_centers_controller.rb
@@ -1,19 +1,34 @@
 class ResearchCentersController < ApplicationController
   def index
+    @research_centers = ResearchCenter.all
   end
 
   def show
   end
-
-  def create
+  
+  def new
+    @research_center = ResearchCenter.new
   end
 
-  def new
+  def create
+    @research_center = ResearchCenter.new(research_center_params)
+
+    if @research_center.save
+      redirect_to research_centers_path, notice: "Successfully added."   
+    else   
+      render "new"   
+    end   
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private 
+
+  def research_center_params
+    params.require(:research_center).permit(:name)
   end
 end
