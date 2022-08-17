f510c9b3906c1f54e1b33f5e553e8c4189162a08
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-17 14:14:06 +0300

    Migrate research centers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_084522) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_111318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_084522) do
     t.datetime "updated_at", null: false
     t.text "abstract"
     t.index ["user_id"], name: "index_proposals_on_user_id"
+  end
+
+  create_table "research_centers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
