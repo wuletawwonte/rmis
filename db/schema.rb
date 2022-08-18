884b27bb6fa771b2730443343cf068f793fd200e
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-18 14:54:16 +0300

    Migrate status column adding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_112916) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_115247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_112916) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["proposal_id"], name: "index_members_on_proposal_id"
     t.index ["user_id"], name: "index_members_on_user_id"
   end
