3848e28e70b428248300dd36835e975675c97bd7
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-20 11:27:22 +0300

    Install simple_form gem

--- Gemfile
+++ Gemfile
@@ -32,3 +32,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem 'simple_form'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :proposals, through: :members 
   has_many :research_centers
   has_many :themes
+
+  validates :first_name, presence: true
 end
