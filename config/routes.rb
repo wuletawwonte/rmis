eaa72bae9ce1e4ed0cc5284f9dad9f6e84225550
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-22 21:14:23 +0300

    Change root route both for authenticated and unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,18 @@ Rails.application.routes.draw do
   resources :research_centers, only: %i[index show create new edit destroy]
   resources :proposals, only: %i[index show new create edit destroy]
   
-  root 'users#index'
-
   get 'members/create'
   delete '/users/sign_out', to: 'users#sign_out'
   get '/users', to: redirect('/users/sign_up')
+
+  devise_scope :user do
+    unauthenticated :user do
+      root "devise/sessions#new"
+    end
+
+    authenticated :user do
+      root "users#index", as: :authenticated_root
+    end
+  end
 
 end
