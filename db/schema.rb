613f3408a584fc63e94a47f06772f7155a5a0c5b
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-25 19:50:47 +0300

    Generate migration to remove theme column from proposals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_115247) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_164948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_115247) do
     t.bigint "user_id", null: false
     t.string "title"
     t.string "attachement"
-    t.string "theme"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
