ea6f6af827e5829694e0415c69999c7f9eba2c91
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-27 04:00:05 +0300

    Add sex to users table as a column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_005149) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_005929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_005149) do
     t.string "first_name"
     t.string "middle_name"
     t.string "last_name"
+    t.string "sex"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
