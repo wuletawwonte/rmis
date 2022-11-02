9c934ddb8e51afe450a6cb9810ca37e1d2ba08f0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-02 08:54:44 +0300

    Add date format to show in subscribers list

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,4 +1,6 @@
 class SubscribersController < ApplicationController
+  # GET /subscribers or /subscribers.json
   def index
+    @subscribers = Subscriber.all.order(:created_at).page params[:page]
   end
 end
