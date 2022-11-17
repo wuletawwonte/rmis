cdbea1bd6e7aa5136e8e29fca39d681d53a0654a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-17 16:59:04 +0300

    Create a view for the call_posted_email mailer action

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,2 +1,7 @@
 class SubscriberMailer < ApplicationMailer
+  def call_posted_email
+    @user = params[:user]
+    @url = 'https://rmis.amu.edu.et';
+    mail(to: @user.email, subject: 'A call is posted'); 
+  end
 end
