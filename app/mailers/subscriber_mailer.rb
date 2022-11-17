3423b281e67574a2936220ec8d57d38a3163803f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-17 17:03:22 +0300

    Create text based email view for call_posted_email mailer action

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,6 +1,8 @@
 class SubscriberMailer < ApplicationMailer
+  
   def call_posted_email
     @user = params[:user]
+    @call = params[:call]
     @url = 'https://rmis.amu.edu.et';
     mail(to: @user.email, subject: 'A call is posted'); 
   end
