f589b8afc29e54f283903c6d621247f24bfd333f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-18 15:45:46 +0300

    Create a method to add Random hash to each email subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,8 @@
 class Subscriber < ApplicationRecord
+  before_create :add_subscription_hash
+
+  private
+    def add_subscription_hash
+      self.subscription_hash = SecureRandom.hex
+    end
 end
