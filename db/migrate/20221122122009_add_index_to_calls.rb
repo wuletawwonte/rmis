aeafc55d5f4ce1ee8fa185eb5535d3278e40b171
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-22 15:20:48 +0300

    Add code unique column to calls table

---
+++ db/migrate/20221122122009_add_index_to_calls.rb
@@ -0,0 +1,6 @@
+class AddIndexToCalls < ActiveRecord::Migration[7.0]
+  def change
+    add_column :calls, :code, :string
+    add_index :calls, :code, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_123615) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_122009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_123615) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "code"
+    t.index ["code"], name: "index_calls_on_code", unique: true
     t.index ["user_id"], name: "index_calls_on_user_id"
   end
 
