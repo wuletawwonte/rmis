7e3cc7cda87d85dd97d8bbf669ee0d1654e61679
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-23 09:43:50 +0300

    Generate migration to remove all columns of research types table except name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_122009) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_064303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,10 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_122009) do
 
   create_table "research_types", force: :cascade do |t|
     t.string "name"
-    t.integer "allowed_per_year"
-    t.boolean "theme_based"
-    t.string "fund"
-    t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_research_types_on_name", unique: true
