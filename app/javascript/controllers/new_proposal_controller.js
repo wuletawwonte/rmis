4a90b356286c10063b100c7e88ff6b0e8a420365
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-24 17:32:57 +0300

    Unhide research calls field on change of research type field

--- app/javascript/controllers/new_proposal_controller.js
+++ app/javascript/controllers/new_proposal_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+  static targets = ['call'];
+
   connect() {
     console.log('New proposal controller');
+  }
+
+  makeChanges() {
+    console.log('Make is done');
+    this.callTarget.classList.remove('hidden');
   }
 }
