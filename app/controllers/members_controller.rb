44f65b7989f5b7e08b789d9661db99fa562c9dc3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-29 20:07:29 +0300

    Change link_to to button_to for member envitation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,19 @@
 class MembersController < ApplicationController
-  def create; 
+  def create
+    @member = Member.new(member_params);
+    @member.role = Member.roles[:co_investigator];
+    @member.status = Member.statuses[:envited];
+    
+    if @member.save
+      redirect_to proposals_path, notice: 'Member successfully added.'
+    else 
+      redirect_to proposals_path, notice: 'unable to add Member.'
+    end
+
   end
+
+  private 
+    def member_params
+      params.permit(:proposal_id, :user_id)
+    end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   belongs_to :user
   belongs_to :proposal
 
-  enum :role, { coinvestigator: 0, principal_investigator: 1 }
+  enum :role, { co_investigator: 0, principal_investigator: 1 }
   enum :status, { joined: 0, envited: 1 }
 
 end
