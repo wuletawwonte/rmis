8b09f85756abcf76739f4786824af1f11543cab2
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-02 21:52:01 +0300

    Migrate education level model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_140026) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_185035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_140026) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
+  create_table "education_levels", force: :cascade do |t|
+    t.string "name"
+    t.integer "order_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_education_levels_on_name", unique: true
   end
 
   create_table "members", force: :cascade do |t|
