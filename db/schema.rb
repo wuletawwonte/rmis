cf028dcc142216c64c603d91c4ae41e3fac58c2d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-02 17:03:39 +0300

    Create migration to recreate academic_rank table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_141307) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_140026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "academic_ranks", force: :cascade do |t|
+    t.string "name"
+    t.integer "order_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_academic_ranks_on_name", unique: true
+  end
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
