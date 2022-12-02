771bca36383a3e58f29f4c266b64a976f3d74006
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-03 02:38:32 +0300

    Migrate department scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_185035) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_233757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_185035) do
     t.string "code"
     t.index ["code"], name: "index_calls_on_code", unique: true
     t.index ["user_id"], name: "index_calls_on_user_id"
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_departments_on_name", unique: true
   end
 
   create_table "documents", force: :cascade do |t|
