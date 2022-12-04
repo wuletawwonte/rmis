0020fe9b8daefd6e56a740e5131ed589a216ce91
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-04 22:49:53 +0300

    Add homepage to ability for authorization by adding actions of Calls controller

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,25 +4,33 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Call, published: true
+    can :list, Call
+    can :public_show, Call
+    can :list, Document
 
-    return unless user.present?
-    can :read, Theme
-    can :read, ResearchType
-    can :read, ResearchCenter
-    can :read, Call
-    can :read, User
+    if user.present?
+      if user.admin?
+        can :manage, User
+        can :manage, Theme
+        can :manage, ResearchType
+        can :manage, ResearchCenter
+        can :manage, Faculty
+        can :manage, EducationLevel
+        can :manage, Document
+        can :manage, Department
+        can :manage, Call
+        can :manage, AcademicRank
+        can :read, Proposal
 
-    return unless user.admin?
-    can :manage, User
-    can :manage, Theme
-    can :manage, ResearchType
-    can :manage, ResearchCenter
-    can :manage, Faculty
-    can :manage, EducationLevel
-    can :manage, Document
-    can :manage, Department
-    can :manage, Call
-    can :manage, AcademicRank
+      else
+        can :read, Theme
+        can :read, ResearchType
+        can :read, ResearchCenter
+        can :read, Call
+        can :manage, User, user: user
+        can :manage, Proposal, user: user
+      end
+    end
+
   end
 end
