81f4570691932a8f5434e299e20edc56359a72a8
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-05 21:48:22 +0300

    Fix profile picture style of all appearances

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,9 @@
 module UsersHelper
-  def avatar_for(user, size: 24 )
+  def avatar_for(user, size: 24, extra_class: "" )
     if user.avatar.attached?
-      content_tag(:div, image_tag(user.avatar, alt: user.full_name, class: "w-#{size} h-#{size} rounded-full"), class: "image overflow-hidden flex justify-center")
+      content_tag(:div, image_tag(user.avatar, alt: user.full_name, class: "rounded-full object-cover w-#{size} h-#{size}"), class: "flex items-center justify-center rounded-full bg-clip-content box-border border-2 p-1 border-#{user.color}-600 #{extra_class}")
     else
-      link_to user_path(user), class: "flex items-center text-sm gap-2  justify-center" do 
-        content_tag(:div, content_tag(:span, user.initials, class: "font-medium text-gray-700"), class: "inline-flex overflow-hidden relative justify-center items-center bg-slate-200 rounded-full bg-clip-content box-content w-#{size} h-#{size} border-2 p-1 border-#{user.color}-600")        
-      end
+      content_tag(:div, content_tag(:span, user.initials, class: "font-medium text-gray-700"), class: "flex justify-center inline-flex overflow-hidden relative justify-center items-center bg-slate-200 rounded-full bg-clip-content box-content w-#{size} h-#{size} border-2 p-1 border-#{user.color}-600 #{extra_class}")        
     end
   end
 end
