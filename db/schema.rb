696d60158664d4803614528b79ad872ad85962bf
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-05 16:02:03 +0300

    Add color to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_142411) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_130123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -200,6 +200,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_142411) do
     t.integer "sex", default: 0
     t.string "role", default: "researcher"
     t.bigint "profile_id"
+    t.string "color"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
