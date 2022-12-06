b2d72d159adf3d9117a7ed1e5aa6e76ff891a272
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-06 11:01:15 +0300

    Add shadow for mini cards in the dashboard page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def avatar_for(user, size: 24, extra_class: '')
     if user.avatar.attached?
       content_tag(:div, image_tag(user.avatar, alt: user.full_name, class: "rounded-full object-cover w-#{size} h-#{size}"),
-                  class: "flex items-center justify-center rounded-full bg-clip-content box-border border-2 p-1 border-#{user.color}-600 #{extra_class}")
+                  class: "flex items-center justify-center rounded-full bg-clip-content box-content border-2 p-1 border-#{user.color}-600 #{extra_class}")
     else
       content_tag(:div, content_tag(:span, user.initials, class: 'font-medium text-gray-700'),
                   class: "flex justify-center inline-flex overflow-hidden relative justify-center items-center bg-slate-200 rounded-full
