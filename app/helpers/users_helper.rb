5e87f6ad0c5e774b24c510ac243be93c6b7266af
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-06 12:37:35 +0300

    Fix too big font size of initials in an avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
-  def avatar_for(user, size: 24, extra_class: '')
+  def avatar_for(user, size: 24, div_class: '', span_class: '')
     if user.avatar.attached?
       content_tag(:div, image_tag(user.avatar, alt: user.full_name, class: "rounded-full object-cover w-#{size} h-#{size}"),
-                  class: "flex items-center justify-center rounded-full bg-clip-content box-content border-2 p-1 border-#{user.color}-600 #{extra_class}")
+                  class: "flex items-center justify-center rounded-full bg-clip-content box-content border-2 p-1 border-#{user.color}-600 #{div_class}")
     else
-      content_tag(:div, content_tag(:span, user.initials, class: 'font-medium text-gray-700'),
+      content_tag(:div, content_tag(:span, user.initials, class: "font-medium text-gray-700 #{span_class}"),
                   class: "flex justify-center inline-flex overflow-hidden relative justify-center items-center bg-slate-200 rounded-full
-                    bg-clip-content box-content w-#{size} h-#{size} border-2 p-1 border-#{user.color}-600 #{extra_class}")
+                    bg-clip-content box-content w-#{size} h-#{size} border-2 p-1 border-#{user.color}-600 #{div_class}")
     end
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,7 +19,6 @@ class Ability
         can :manage, Department
         can :manage, Call
         can :manage, AcademicRank
-        can :read, Proposal
 
       else
         can :read, Theme
