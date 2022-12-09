ce07ed7da108fc6f3dabee71378a33bd4655e57a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-09 15:06:40 +0300

    Create modal to show details of each researcher

--- app/assets/config/init-alpine.js
+++ app/assets/config/init-alpine.js
@@ -41,15 +41,5 @@ function data() {
           console.log(err);
         });
     },
-    isModalOpen: false,
-    trapCleanup: null,
-    openModal() {
-      this.isModalOpen = true;
-      this.trapCleanup = focusTrap(document.querySelector('#modal'));
-    },
-    closeModal() {
-      this.isModalOpen = false;
-      this.trapCleanup();
-    },
   };
 }
