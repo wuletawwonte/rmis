66b1a3ea0c2dd4ab1ab142bce753385ad3b292c1
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-26 10:27:53 +0300

    Remove subscribers status

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,13 +8,3 @@ import 'Chart.bundle';
 import Highcharts from 'highcharts';
 
 window.Highcharts = Highcharts;
-
-const subscribersCheckboxs = document.querySelectorAll('.subscriber-checkbox');
-
-subscribersCheckboxs.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    fetch(`http://localhost:3000/subscribers/${e.target.id}.json`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  });
-});
