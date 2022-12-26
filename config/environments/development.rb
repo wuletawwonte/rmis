9a8a297acd69c3da1b70a8c5d1f655b5b223a0f6
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-26 10:48:31 +0300

    Add email creds in the code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,21 +44,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # SMTP configuration of action mailer
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gmail.com',
-  #   user_name:            Rails.application.credentials.dig(:email, :username),
-  #   password:             Rails.application.credentials.dig(:email, :password),
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true,
-  #   open_timeout:         5,
-  #   read_timeout:         5 
-  # }
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'gracesoftwebdesign@gmail.com',
+    password:             'phantome81',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
