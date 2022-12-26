fdabb1f8876e7a74042c5787978d27e2f08f840d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-26 15:22:34 +0300

    Add proposal send button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "rmis.amu.edu.et"
-  # config.action_mailer.default_url_options = { host: 'rmis.amu.edu.et', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
