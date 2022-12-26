87ee2e0810e2fcf78d70915a6578c2b67a934824
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-26 10:52:38 +0300

    Add rmis domain name to the environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -87,5 +87,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "rmis.amu.edu.et"
+  config.action_mailer.default_url_options = { host: 'rmis.amu.edu.et', port: 3000 }
 end
