e001e0cc5945434731ff756979d554d67686b508
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-01-31 15:45:49 +0300

    Seed general settings table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.destroy_all
+GlobalSetting.destroy_all
 
 # Get email of a user with amu.edu.et domain
 def user_email(fname, lname)
@@ -57,3 +58,25 @@ end
 
 create_researchers
 puts "5 researcher accounts created and ready"
+
+def generate_global_setting
+  GlobalSetting.create(
+    allowed_co_per_year: 2,
+    allowed_pi_per_year: 2,
+    date_format: 'dd-mm-yyyy',
+    default_password: '123456',
+    minimum_password_length: 6,
+    university_city: 'Arba Minch',
+    university_country: 'ET',
+    university_email: 'corcom@amu.edu.et',
+    university_facebook: 'https://www.facebook.com/arbaminch_university',
+    university_name: 'Arba Minch University',
+    university_phone: '+251468810243',
+    university_telegram: 'telegram.app/amu',
+    university_twitter: 'https://twitter.com/amu',
+    university_website: 'https://www.amu.edu.et'
+  )
+end
+
+generate_global_setting
+puts "default global settings value generated"
