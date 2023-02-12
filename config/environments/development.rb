df0c5ff7052ad903be0a849748d4fb424faabdef
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-02-12 13:24:05 +0300

    Add allowed request origins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_cable.allowed_request_origins = [/https:\/\/*/]
+
   config.hosts << "rmis-production.up.railway.app"
 
 end
