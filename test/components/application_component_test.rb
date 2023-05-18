f6be65d57891f007cf9d85eb38987a2d9916c330
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-05-18 11:06:01 +0300

    Add ApplicationComponent view component

---
+++ test/components/application_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ApplicationComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(ApplicationComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
