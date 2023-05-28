203160b50cd0e291cf4f0fc57c6dea092eb8116a
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-05-28 20:24:20 +0300

    WIP: add tooltip parameter to the button view component

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -13,15 +13,16 @@ class ButtonComponent < ApplicationViewComponent
     sm: 'px-3 py-1 text-sm font-medium leading-5'
   }.with_indifferent_access
 
-  param :label  # Can be a string or an icon component
-  param :href, optional: true   # If present, the button will be an anchor tag
-  option :icon, optional: true  # If present, the button will be an icon button
-  option :size, default: proc { :md }   # :sm, :md, :lg
-  option :type, default: proc { :primary }  # :primary, :secondary
+  param :label # Can be a string or an icon component
+  param :href, optional: true # If present, the button will be an anchor tag
+  option :icon, optional: true # If present, the button will be an icon button
+  option :size, default: proc { :md } # :sm, :md, :lg
+  option :type, default: proc { :primary } # :primary, :secondary
   option :full_rounded, default: proc { false } # If true the border will be full round
   option :left_icon, optional: true # If present, the button will have an icon on the left
   option :right_icon, optional: true # If present, the button will have an icon on the right
   option :submit, default: proc { false } # If true the button will be a submit button
+  option :tooltip, optional: true # If present, the button will have a tooltip
 
   def parent_tag(&)
     button_classes = class_names(
@@ -33,7 +34,8 @@ class ButtonComponent < ApplicationViewComponent
 
     parent_tag_options = { class: button_classes }
     parent_tag_options[:href] = resolve_url(href) if href
-    parent_tag_options[:type] = "submit" if submit == true && href.nil?
+    parent_tag_options[:type] = 'submit' if submit == true && href.nil?
+    parent_tag_options[:title] = tooltip if tooltip
 
     content_tag(html_tag, **parent_tag_options, &)
   end
