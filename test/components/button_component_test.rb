ba1f45b827c1b8cdc6137cb2a17df9208d4d19a0
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-14 12:37:05 +0300

    Fix rubocop linter errors

--- test/components/button_component_test.rb
+++ test/components/button_component_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class ButtonComponentTest < ViewComponent::TestCase
   def test_render_component
-    render_inline(ButtonComponent.new("home", "#"))
-    
-    assert_selector "span", text: "home"
+    render_inline(ButtonComponent.new('home', '#'))
+
+    assert_selector 'span', text: 'home'
   end
 end
