596d30fbde664a977f721b344e640d0288c620ed
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-15 11:56:05 +0300

    Get rid of unnecessary test files

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
+  load_and_authorize_resource
+  
   def index
     @last_users = User.last(5).reverse
     @users = User.where.not(role: 'admin').order('created_at desc').page params[:page]
