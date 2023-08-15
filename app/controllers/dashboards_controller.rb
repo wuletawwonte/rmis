2a16b2e793394e9a25d1b537726d456ff40c4eff
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-15 11:49:34 +0300

    Change dashboard page of login users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class DashboardsController < ApplicationController
   def index
+    @last_users = User.last(5).reverse
+    @users = User.where.not(role: 'admin').order('created_at desc').page params[:page]
   end
 end
--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get dashboards_index_url
     assert_response :success
   end
