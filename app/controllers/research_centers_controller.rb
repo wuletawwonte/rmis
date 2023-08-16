957edfbf06327a46576268803dba4a261723a6e2
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-16 10:39:21 +0300

    Refactor research centers in the researcher dashboard

---
+++ app/controllers/research_centers_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class ResearchCentersController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_research_center, only: %i[show]
+
+  def index
+    @research_centers = ResearchCenter.order(:created_at).page params[:page]
+  end
+
+  def show; end
+
+  private
+
+  def set_research_center
+    @research_center = ResearchCenter.find(params[:id])
+  end
+end
