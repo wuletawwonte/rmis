589ebaaee2a83a4126362885785af2d750155b07
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-17 12:26:30 +0300

    Get theme from themes.yaml file

--- db/seeds/research_centers.rb
+++ db/seeds/research_centers.rb
@@ -2,10 +2,11 @@
 
 def create_research_centers
   all_research_centers = YAML.load_file(Rails.root.join('db', 'seeds', 'research_centers.yml'))
-  all_research_centers.sample(3).each do |item|
+  themes = YAML.load_file(Rails.root.join('db', 'seeds', 'themes.yml'))
+  all_research_centers.sample(3).each_with_index do |item, index|
     person = SeedsHelper.random_person
 
-    research_center = ResearchCenter.create(
+    research_center = ResearchCenter.new(
       name: item
     )
     research_center.build_user(
@@ -17,6 +18,9 @@ def create_research_centers
       sex: person.gender,
       role: 'research_coordinator'
     )
+    themes[index].each do |theme_item|
+      Theme.create(title: theme_item, research_center:, user: research_center.user)
+    end
     research_center.save
   end
 end
