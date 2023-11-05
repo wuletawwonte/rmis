608d0782b184dc47397682e810101d80719d7254
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-05 20:41:56 +0300

    Add support for classes for icon helper

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,8 +1,10 @@
 module IconsHelper
-  def icon(name, options = {})
-    default_options = {class: "fill-current", width: "18", height: "18"}
+  def icon(name, **kwargs)
+    default_options = {width: "18", height: "18"}
+    base_class = "fill-current"
 
-    merged_options = default_options.merge(options)
-    inline_svg_tag "lineicons/#{name}.svg", **merged_options
+    klass = kwargs.delete(:class)
+    merged_options = default_options.merge(kwargs)
+    inline_svg_tag "lineicons/#{name}.svg", class: "#{base_class} #{klass}", **merged_options
   end
 end
