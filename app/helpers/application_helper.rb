93ab0965b24ae484236c8422bec9f875a74aea0c
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-06 10:26:24 +0300

    Delete empty helper files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def menu_title(title)
+    content_tag(:h3, title.upcase, class: "mb-4 ml-4 text-sm font-medium text-bodydark2")
+  end
 end
