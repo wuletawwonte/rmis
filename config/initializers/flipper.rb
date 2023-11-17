a4e74c45de0a3503cf21a4492fcf1e7d824b16c6
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-17 20:45:07 +0300

    Configure flipper to use active record as a default adapter

---
+++ config/initializers/flipper.rb
@@ -0,0 +1,9 @@
+# config/initializers/flipper.rb
+require "flipper/adapters/active_record"
+
+Flipper.configure do |config|
+  config.default do
+    adapter = Flipper::Adapters::ActiveRecord.new
+    Flipper.new(adapter)
+  end
+end
