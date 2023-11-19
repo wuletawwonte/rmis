a0d138501c3e587095b8d233f5bf0a5f841860db
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-19 17:45:06 +0300

    Load rake tasks before running them in initializers

--- config/initializers/system_modules_initializer.rb
+++ config/initializers/system_modules_initializer.rb
@@ -1,4 +1,5 @@
 Rails.application.config.after_initialize do
   # The code to run the Rake task that loads data from the YAML file and populate the database.
+  Rails.application.load_tasks
   Rake::Task["system_module:populate"].invoke
 end
