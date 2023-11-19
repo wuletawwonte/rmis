9e3d0b08abe7480d07974cb48f2d273e540d28bf
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-19 17:23:33 +0300

    Fix rails test error

--- config/initializers/system_modules_initializer.rb
+++ config/initializers/system_modules_initializer.rb
@@ -1,4 +1,4 @@
 Rails.application.config.after_initialize do
-  # Your code here to run the Rake task or load the data from the YAML file
-  Rake::Task["system_modules:populate"].invoke
+  # The code to run the Rake task that loads data from the YAML file and populate the database.
+  Rake::Task["system_module:populate"].invoke
 end
