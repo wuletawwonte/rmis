4fc87da214a23efcb2900b9798b93c835f8a232d
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-19 18:51:34 +0300

    Add after initialize check for db:schema:load

--- config/application.rb
+++ config/application.rb
@@ -32,10 +32,12 @@ module Rmis
     config.importmap.cache_sweepers << Rails.root.join("app/components")
 
     # Populate system_modules table with data from YAML file
-    # config.after_initialize do
-    # The code to run the Rake task that loads data from the YAML file and populate the database.
-    # Rails.application.load_tasks
-    # Rake::Task["system_module:populate"].invoke
-    # end
+    config.after_initialize do
+      unless ARGV.include?("db:schema:load")
+        # The code to run the Rake task that loads data from the YAML file and populate the database.
+        Rails.application.load_tasks
+        Rake::Task["system_module:populate"].invoke
+      end
+    end
   end
 end
