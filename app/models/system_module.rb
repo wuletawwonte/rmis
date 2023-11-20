81d71adef0c8af57523e493d2cff3080a78385fe
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-20 11:48:54 +0300

    Render sidebar menu items based on module status check

--- app/models/system_module.rb
+++ app/models/system_module.rb
@@ -15,11 +15,20 @@
 #  index_system_modules_on_key  (key) UNIQUE
 #
 class SystemModule < ApplicationRecord
+
+  after_update :clear_cache
+
   validates :key, presence: true, uniqueness: true
 
   def self.enabled?(module_key)
     Rails.cache.fetch("system_module_#{module_key}", expires_in: 1.hour) do
       !!find_by(key: module_key)&.enabled?
     end
+  end
+
+  private
+
+  def clear_cache
+    Rails.cache.delete("system_module_#{key}")
   end
 end
