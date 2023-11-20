99e1fe87498fa6069dbc0d70b4076a36c32bf372
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-11-20 15:25:04 +0300

    Set application version in application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Rmis
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Set version of the application
+    config.version = "1.0.0"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
